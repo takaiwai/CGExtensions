b29c43ceca7378a4b82980e6350fc383acafd84c
Taka Iwai <hello@takaiwai.com>
2017-01-07 12:09:54 -0800

    Implement CGVector.init(angle:) and CGVector.init(angle:length:)

--- CGExtensions/Classes/CGVector+Extensions.swift
+++ CGExtensions/Classes/CGVector+Extensions.swift
@@ -5,5 +5,13 @@ extension CGVector {
     public init(point: CGPoint) {
         self.init(dx: point.x, dy: point.y)
     }
+
+    public init(angle radians: CGFloat) {
+        self.init(angle: radians, length: 1.0)
+    }
+
+    public init(angle radians: CGFloat, length: CGFloat) {
+        self.init(dx: cos(radians) * length, dy: sin(radians) * length)
+    }
     
 }
--- Example/Tests/CGVectorExtensionsSpec.swift
+++ Example/Tests/CGVectorExtensionsSpec.swift
@@ -13,6 +13,48 @@ class CGVectorExtensionsSpec: QuickSpec {
                 }
             }
 
+            describe("init(angle:)") {
+                it("returns an unit vector which has the specified angle in radians") {
+                    let v1 = CGVector(angle: 0.0)
+                    expect(v1) == CGVector(dx: 1.0, dy: 0.0)
+
+                    let v2 = CGVector(angle: CGFloat(30.0).degreesInRadians)
+                    expect(v2.dx) ≈ 0.8660254
+                    expect(v2.dy) ≈ 0.5
+
+                    let v3 = CGVector(angle: CGFloat(135.0).degreesInRadians)
+                    expect(v3.dx) ≈ -0.70710677
+                    expect(v3.dy) ≈ 0.70710677
+                }
+
+//                it("is a unit vector") {
+//                    let v1 = CGVector(angle: 0.0)
+//                    let v2 = CGVector(angle: CGFloat(30.0).degreesInRadians)
+//                    let v3 = CGVector(angle: CGFloat(135.0).degreesInRadians)
+//                }
+            }
+
+            describe("init(angle:length:)") {
+                it("returns a vector which has the specified angle in radians, and length") {
+                    let v1 = CGVector(angle: 0.0, length: 3.5)
+                    expect(v1) == CGVector(dx: 3.5, dy: 0.0)
+
+                    let v2 = CGVector(angle: CGFloat(30.0).degreesInRadians, length: 25.0)
+                    expect(v2.dx) ≈ 21.650635
+                    expect(v2.dy) ≈ 12.5
+
+                    let v3 = CGVector(angle: CGFloat(135.0).degreesInRadians, length: 5.0)
+                    expect(v3.dx) ≈ -3.535534
+                    expect(v3.dy) ≈ 3.535534
+                }
+
+//                it("has the specified length") {
+//                    let v1 = CGVector(angle: 0.0, length: 3.5)
+//                    let v2 = CGVector(angle: CGFloat(30.0).degreesInRadians, length: 25.0)
+//                    let v3 = CGVector(angle: CGFloat(135.0).degreesInRadians, length: 5.0)
+//                }
+            }
+
         }
     }
 }
